76f3cdd24f81c397d2473d972b6784f9597fa6b6
Todd H <github@simplybest.com>
2024-02-26 20:34:35 -0700

    add userdata user list to tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, ToastController } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, ToastController, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { person, personOutline } from 'ionicons/icons';
 import { SettingsPage } from '../settings/settings.page';
 import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
+import { collection, where, getDocs, Firestore, query } from '@angular/fire/firestore';
+
+const USER_DATA = 'UserData';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [ NgIf, IonButtons, IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent ],
+  imports: [ IonLabel, IonItem,  NgIf, NgFor, IonButtons, IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent ],
 })
 export class Tab1Page implements OnInit {
   public auth: Auth = inject(Auth);
   private provider: GoogleAuthProvider = new GoogleAuthProvider();
+  private db: Firestore = inject(Firestore);
+  public userList: any[] = [];
   
   constructor(private modalCtrl: ModalController, private toast: ToastController) {
     addIcons({ personOutline, person });
@@ -23,9 +28,14 @@ export class Tab1Page implements OnInit {
 
   ngOnInit() {
     this.auth.authStateReady().then(() => {}, () => {});
+    this.refreshPage();
   }
 
   refreshPage() {
+    const q = query(collection(this.db, USER_DATA), where("seeName", "==", true));
+    getDocs(q).then((querySnapshot) => {
+      this.userList = querySnapshot.docs.map(doc => doc.data());
+    }, () => {});
   }
 
   async openUserSettings() {
