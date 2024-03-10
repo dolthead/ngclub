81184c2b89260fd95bf282968272d899b81f611e
Todd H <github@simplybest.com>
2024-03-09 22:05:11 -0700

    move firebase access to services, add model

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,80 +1,50 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { closeCircleOutline } from 'ionicons/icons';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, ToastController, IonAvatar, IonCard, IonCardContent, IonCheckbox, IonLabel, IonCardHeader } from '@ionic/angular/standalone';
-import { Auth, User } from '@angular/fire/auth';
-import { CollectionReference, DocumentData, Firestore, addDoc, collection, doc, getDocs, query, setDoc, where } from '@angular/fire/firestore';
-
-const USER_DATA = 'UserData';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, IonAvatar, IonCard, IonCardContent, IonCheckbox, IonLabel, IonCardHeader, IonItem, IonList } from '@ionic/angular/standalone';
+import { Auth } from '@angular/fire/auth';
+import { UserSettings } from 'src/models/user-settings.model';
+import { UserSettingsService } from 'src/services/user-settings.service';
+import { AppToastService } from 'src/services/app-toast.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonCardHeader, IonLabel,  CommonModule, FormsModule, IonButtons, IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonAvatar, IonCard, IonCardContent, IonCheckbox ],
+  imports: [IonList, IonItem,  NgIf, IonCardHeader, IonLabel, CommonModule, FormsModule, IonButtons, IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonAvatar, IonCard, IonCardContent, IonCheckbox ],
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
   private auth: Auth = inject(Auth);
-  public user: User | null;
-  public currentDoc: DocumentData | undefined = undefined;
-  public settings: any = { 
-    displayName: undefined,
-    photoURL: undefined,
-    uid: undefined, 
-    seeName: false, 
-    allowChat: false, 
-    allowInvites: false, 
-    allowPhotoTags: false,
-  };
-  
-  private db: Firestore = inject(Firestore);
-  public userCollection: CollectionReference = collection(this.db, USER_DATA);
+  private modalCtrl: ModalController = inject(ModalController);
+  private settingsService: UserSettingsService = inject(UserSettingsService);
+  private toastCtrl: AppToastService = inject(AppToastService);
+  public settings: void | UserSettings = undefined;
 
-  constructor(private modalCtrl: ModalController, private toast: ToastController) { 
+  constructor() {
     addIcons({ closeCircleOutline });
-    this.user = this.auth.currentUser;
+  }
 
-    const q = query(collection(this.db, USER_DATA), where("uid", "==", this.user?.uid));
-    getDocs(q).then((querySnapshot) => {
-      this.currentDoc = querySnapshot.docs.length ? querySnapshot.docs[0] : undefined;
-      if (this.currentDoc) {
-        this.settings = this.currentDoc['data']();
-      } else {
-        this.settings.displayName = this.user?.displayName;
-        this.settings.photoURL = this.user?.photoURL;
-        this.settings.uid = this.user?.uid;
-        addDoc(this.userCollection, this.settings)
-          .then(doc => this.currentDoc = doc, () => {});
-      }
-    }, () => {})
+  ngOnInit(): void {
+    this.settingsService.getUserSettings(this.auth.currentUser)
+      .then(data => this.settings = data, () => {});
   }
 
   saveSettings() {
-    if (this.currentDoc) {
-      setDoc(doc(this.db, USER_DATA, this.currentDoc['id']), this.settings);
-    }
+    this.settingsService.saveUserSettings(this.settings);
   }
 
-  close() { 
+  close() {
     this.modalCtrl.dismiss();
   }
 
   logout() {
     this.auth.signOut()
       .then(() => {
-        this.toast.create({
-          message: 'Logged out successfully',
-          duration: 5000,
-        }).then(toast => toast.present());
+        this.toastCtrl.toast({ message: 'Logged out successfully' });
         this.modalCtrl.dismiss();
-      }, (error) => {
-        this.toast.create({
-          message: error.message,
-          duration: 5000,
-        }).then(toast => toast.present());
-      });
+      }, error => this.toastCtrl.toast({ message: error.message }));
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, ToastController, LoadingController, IonItem, IonLabel, IonList, IonAvatar, IonRefresher, IonRefresherContent, IonSkeletonText } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, ModalController, IonItem, IonLabel, IonList, IonAvatar, IonRefresher, IonRefresherContent, IonSkeletonText } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { person, personOutline } from 'ionicons/icons';
 import { SettingsPage } from '../settings/settings.page';
 import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
 import { NgIf, NgFor, NgStyle } from '@angular/common';
-import { collection, where, getDocs, Firestore, query, DocumentData } from '@angular/fire/firestore';
-
-const USER_DATA = 'UserData';
+import { DocumentData } from '@angular/fire/firestore';
+import { UserDataService } from 'src/services/user-data.service';
+import { AppToastService } from 'src/services/app-toast.service';
 
 @Component({
   selector: 'app-tab1',
@@ -17,14 +17,17 @@ const USER_DATA = 'UserData';
   imports: [ IonSkeletonText, IonRefresherContent, IonRefresher, IonAvatar, IonList,  IonLabel, IonItem,  NgIf, NgFor, NgStyle, IonButtons, IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent ],
 })
 export class Tab1Page implements OnInit {
-  public auth: Auth = inject(Auth);
   private provider: GoogleAuthProvider = new GoogleAuthProvider();
-  private db: Firestore = inject(Firestore);
-  public userList: DocumentData[] = [];
+  private userData: UserDataService = inject(UserDataService);
+  private toastService: AppToastService = inject(AppToastService);
+  private modalCtrl: ModalController = inject(ModalController);
+  
+  public auth: Auth = inject(Auth);
+  public userList: undefined | DocumentData[] = [];
   public authReady = false;
   public loading = false;
   
-  constructor(private modalCtrl: ModalController, private toast: ToastController) {
+  constructor() {
     addIcons({ personOutline, person });
   }
 
@@ -39,10 +42,8 @@ export class Tab1Page implements OnInit {
     this.userList = [];
     if (this.auth.currentUser) {
       this.loading = true;
-      const q = query(collection(this.db, USER_DATA), where("seeName", "==", true));
-      getDocs(q)
-        .then((querySnapshot) => this.userList = querySnapshot.docs.map(doc => doc.data()), 
-          () => {})
+      this.userData.getUserList()
+        .then((data: undefined | DocumentData[]) => this.userList = data, () => {})
         .finally(() => {
           this.loading = false;
           setTimeout(() => event?.detail?.complete(), 500);
@@ -54,24 +55,15 @@ export class Tab1Page implements OnInit {
 
   async openUserSettings() {
     if (!this.auth.currentUser) {
-      signInWithPopup(this.auth, this.provider)
-        .then(data => {
-          this.openUserSettings();
-          this.toast.create({
-            message: 'Logged in successfully',
-            duration: 5000,
-          }).then(toast => toast.present());
-        }, (error) => {
-          this.toast.create({
-            message: error.message,
-            duration: 5000,
-          }).then(toast => toast.present());
-        });
-    } else {
+      await signInWithPopup(this.auth, this.provider)
+        .then(() => this.toastService.toast({ message: 'Logged in successfully' }),
+          error => this.toastService.toast({ message: error.message }));
+    }
+    if (this.auth.currentUser) {
       const modal = await this.modalCtrl.create({
         component: SettingsPage,
-        initialBreakpoint: 1.0,
-        breakpoints: [0, 0.6, 1.0],
+        initialBreakpoint: 0.9,
+        breakpoints: [0, 0.6, 0.9],
       });
       modal.present();
       await modal.onDidDismiss();
